cbcea7aa6c4760e20e98dbf5ed334fe3c50db37e
Viraj Ghadigaonkar <120920814+virajghadigaonkar99@users.noreply.github.com>
2024-08-16 20:45:53 +0530

    Update settings.py

--- Core/settings.py
+++ Core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3txfbj@piqrq(sw=ex@u5^47r@h74kgc4s!v^ly%(orog)%-_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
